aa967e7f636723493f8958a9bc6427e20dc014b2
SoniaTual <sonia.tual@orange.fr>
2015-11-04 12:02:21 +0100

    enchainement des sequences

--- scripts/game.js
+++ scripts/game.js
@@ -1,5 +1,5 @@
 var compteur=1;
-var nbsequence;
+var nbsequence=1;
 var sequence;
 var nameLevel;
 var sizeSeq;
@@ -8,9 +8,13 @@ var audio_bonne = new Audio('../sons/bonne.mp3');
 var audio_mauvaise = new Audio('../sons/mauvaise.mp3');
 var audio_applaudissement = new Audio('../sons/applause.mp3');
 
+/*function sleep(milliSeconds){
+	var startTime = new Date().getTime();
+	while (new Date().getTime() < startTime + milliSeconds);
+}*/
+
 function init(){
 
-	nbsequence = 1;
 	var url = document.location.href;
 	nameLevel = url.substring(url.lastIndexOf("/")+1);
 	console.log(nameLevel);
@@ -28,7 +32,12 @@ function init(){
 	var level1 = JSON.parse(levelone);
 
 	sizelevel = level1.length;
-
+	 nbsequence =  sessionStorage.getItem("nbsequence");
+	 if(nbsequence == null){
+		 sessionStorage.setItem("nbsequence", 1);
+		 nbsequence =  sessionStorage.getItem("nbsequence");
+	 }
+	 console.log(nbsequence);
 	sequence = level1[nbsequence%sizelevel];
 
 	console.log(sequence);
@@ -39,12 +48,15 @@ function init(){
 
 	var image1=document.getElementById("image1");
 	image1.src = url.concat(sequence[indexRandon[0]]);
+	 //image1.style.display="block";
 
 	var image2=document.getElementById("image2");
 	image2.src = url.concat(sequence[indexRandon[1]]);
+	 //image2.style.display="block";
 
 	var image3=document.getElementById("image3");
 	image3.src = url.concat(sequence[indexRandon[2]]);
+	 //image3.style.display="block";
  }
 
 
@@ -87,6 +99,22 @@ function randomizeArray(arr) {
     return output;
 }
 
+function changeSeq(){
+	nbsequence =  sessionStorage.getItem("nbsequence");
+	console.log(nbsequence);
+	nbsequence++;
+	sessionStorage.setItem("nbsequence", nbsequence);
+	console.log(nbsequence);
+
+	switch (nameLevel){
+		case "niveau0.html": initLevel0();//retour menu
+			break;
+		case "niveau1.html": initLevel1(nbsequence);
+
+			window.location.reload();
+			break;
+	}
+}
 function bonneReponse(e){
 	//Afficher image bonne réponse
 	//son
@@ -102,18 +130,14 @@ function bonneReponse(e){
 	// On affiche l'image oui
 	var oui=document.getElementById("oui");
 	oui.style.display = "block";
-	console.log(compteur);
-	console.log(sequence.length);
 	if (compteur > sizeSeq){
-		audio_applaudissement.play();
-		audio_applaudissement.currentTime = 0;
-		nbsequence++;
-		switch (nameLevel){
-			case "niveau0.html": initLevel0();//retour menu
-				break;
-			case "niveau1.html": initLevel1(nbsequence);
-				break;
+		if(true){
+			audio_applaudissement.play();
+			audio_applaudissement.currentTime = 0;
+
 		}
+
+		changeSeq();
 	}
 	else{
 		audio_bonne.play();
